ff9d17a246b50e28f157ba10b78d58c35000479e
junxian428 <holeyiho@gmail.com>
2021-01-22 13:07:42 +0800

    Initial commit

--- admin.js
+++ admin.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "admin"){
+if ( username == "ho07177" && password == "1234567"){
 alert ("Login successfully");
 window.location = "personal.html"; // Redirecting to other page.
 return false;
